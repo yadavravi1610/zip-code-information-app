83107dd4edada10c624b5bab8c88197c149b2694
Ravinder <108238004+yadavravi1610@users.noreply.github.com>
2023-11-02 14:34:20 +0530

    Adding React Loading Spinner

--- src/ZipCodeInfo.js
+++ src/ZipCodeInfo.js
@@ -1,21 +1,24 @@
 import React, { useState } from 'react'
+import { RotatingLines } from "react-loader-spinner";
 
 function ZipCodeInfo() {
     const [zipCode, setZipcode] = useState('');
+    const [loading, setLoading] = useState(false);
     console.log(zipCode);
 
     async function fetchLocationData(zipCode) {
         try {
             const response = await fetch(`https://api.zippopotam.us/in/${zipCode}`);
             const data = await response.json();
+            setLoading(false);
             console.log(data);
-
         } catch (error) {
             console.log(error.message);
         }
     }
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true);
         fetchLocationData(zipCode);
         setZipcode("");
     }
@@ -28,7 +31,19 @@ function ZipCodeInfo() {
                     setZipcode(e.target.value);
                 }} className='w-72 h-8 border border-black rounded-md pl-2' placeholder='Enter your Zip code' />
                 <button type='submit' className='w-28 h-8 border border-black rounded-md bg-emerald-400 hover:bg-emerald-300'>Submit</button>
+
             </form>
+            {
+                loading && <div className='w-72 h-40'>
+                    <RotatingLines
+                        strokeColor="#febd69"
+                        strokeWidth="5"
+                        animationDuration="0.75"
+                        width="50"
+                        visible={true}
+                    />
+                </div>
+            }
         </div>
     )
 }
