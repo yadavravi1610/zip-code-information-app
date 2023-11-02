8bb64a5c400db92e03c32975c12b7b16b988babe
Ravinder <108238004+yadavravi1610@users.noreply.github.com>
2023-11-02 13:41:44 +0530

    Provide Basic styling

--- src/ZipCodeInfo.js
+++ src/ZipCodeInfo.js
@@ -1,10 +1,11 @@
 import React from 'react'
 const ZipCodeInfo = () => {
   return (
-    <div>
-        <form className='mx-[30%] flex'>
-            <input type='text' placeholder='Enter your Zip code'/>
-            <button type='submit'>Submit</button>
+    <div className='w-auto h-5/6 flex flex-col gap-5 items-center py-40 '>
+        <h1 className='text-4xl text-lime-400 font-semibold'>Enter Your Pin Code</h1>
+        <form className='flex gap-5'>
+            <input type='text' className='w-72 h-8 border border-black rounded-md pl-2' placeholder='Enter your Zip code'/>
+            <button type='submit' className='w-28 h-8 border border-black rounded-md bg-emerald-400 hover:bg-emerald-300'>Submit</button>
         </form>
     </div>
   )
