c774a9c49a3e34784c362dc0aff9778fbd07d35f
Ravinder <108238004+yadavravi1610@users.noreply.github.com>
2023-11-02 17:40:03 +0530

    Added Error Handling and making it responsive

--- src/LocationInfo.js
+++ src/LocationInfo.js
@@ -1,5 +1,3 @@
-
-
 import React from 'react'
 
 const LocationInfo = (props) => {
@@ -7,14 +5,18 @@ const LocationInfo = (props) => {
     console.log(data);
     return (
     <div className='w-40 h-auto'>
-        <h1>Country : {data.country}</h1>
-        <h1>State: {data.places[0].state}</h1>
-        <h1>Place: {data.places[0]['place name']}</h1>
-         {/* <h1>Country : {data[0].PostOffice[0].Country}</h1>
-        <h1>State: {data[0].PostOffice[0].State}</h1>
-        <h1>Place: {data[0].PostOffice[0].District}</h1> */}
+        <h1><span className='font-semibold'>Country </span>: {data.country}</h1>
+        <h1><span className='font-semibold'>State </span>: {data.places[0].state}</h1>
+        <h1><span className='font-semibold'>Place </span>: {data.places[0]['place name']}</h1>
     </div>
   )
 }
 
 export default LocationInfo
+
+
+{/* 
+         First method api
+         <h1>Country : {data[0].PostOffice[0].Country}</h1>
+        <h1>State: {data[0].PostOffice[0].State}</h1>
+        <h1>Place: {data[0].PostOffice[0].District}</h1> */}
--- src/ZipCodeInfo.js
+++ src/ZipCodeInfo.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import LocationInfo from './LocationInfo';
 import { RotatingLines } from "react-loader-spinner";
+import InfoImage from './required.png';
 
 function ZipCodeInfo() {
     const [zipCode, setZipcode] = useState('');
@@ -12,21 +13,20 @@ function ZipCodeInfo() {
 
     async function fetchLocationData(zipCode) {
         try {
-            // const response = await fetch(`https://api.postalpincode.in/pincode/${zipCode}`);
             const response = await fetch(`https://api.zippopotam.us/in/${zipCode}`);
-            if(response.status === 200){
+            if (response.status === 200) {
                 const data = await response.json();
                 setLoading(false);
-                    setLocationData(data);
-                    setList(true);
+                setLocationData(data);
+                setList(true);
             }
-            else{
+            else {
                 setLoading(false);
                 setErrorMessage(true);
             }
-           
+
         } catch (error) {
-            console.log(error.message);
+            setErrorMessage(true);
         }
     }
     const handleSubmit = async (e) => {
@@ -35,13 +35,19 @@ function ZipCodeInfo() {
         fetchLocationData(zipCode);
         setZipcode("");
     }
+    const clear = (e) =>{
+        e.preventDefault();
+        setList(false);
+        setErrorMessage(false);
+    }
 
     return (
-        <div className='w-[70%] my-20 bg-[#ffffff] mx-auto h-auto py-10 flex flex-col gap-5 items-center rounded-md'>
-            <h1 className='text-2xl text-lime-400 font-semibold'>Enter Your Pin Code</h1>
+        <div className='w-[70%] sml:w-[65%] mdl:w-[50%] xl:w-[30%] my-20 bg-[#ffffff] mx-auto h-auto py-10 flex flex-col gap-5 items-center rounded-md'>
+            <h1 className='text-2xl sml:text-3xl mdl:text-4xl text-lime-400 font-semibold'>Enter Your Pin Code</h1>
             <hr />
-            <form className='flex flex-col gap-5' onSubmit={handleSubmit}>
-                <input type='text' maxLength={6} value={zipCode} onChange={(e) => {setZipcode(e.target.value);}} className='w-52 h-8 border border-black rounded-md pl-2' placeholder='Enter your Zip code' />
+            <form className='flex flex-col mdl:flex-row gap-5' onSubmit={handleSubmit}>
+                <input type='text' maxLength={6} value={zipCode} onChange={(e) => { setZipcode(e.target.value);
+                setErrorMessage(false);setList(false); }} className='w-52 h-8 border border-black rounded-md pl-2' placeholder='Enter your Zip code' />
                 <button type='submit' className='mx-auto w-28 h-8 border border-black rounded-md bg-emerald-400 hover:bg-emerald-300'>Submit</button>
             </form>
             {
@@ -59,9 +65,13 @@ function ZipCodeInfo() {
                 list && <LocationInfo data={locationData} />
             }
             {
-                errorMessage && <div className='w-auto h-auto'>
+                errorMessage && <div className='w-auto h-auto flex gap-1 items-baseline'>
+                    <img className='w-3 h-3' src={InfoImage} alt='Info icon' />
                     <h1 className='text-red-500'>Location Not Found</h1>
                 </div>
+            }
+            {
+                (errorMessage || list ) && <button className='w-20 p-1 bg-white border border-black rounded-md hover:bg-gray-200' onClick={clear}>Clear</button> 
             }
 
         </div>
@@ -69,3 +79,12 @@ function ZipCodeInfo() {
 }
 
 export default ZipCodeInfo;
+
+
+ //
+//  By using other Api which provide all the addressess
+//  const response = await fetch(`https://api.postalpincode.in/pincode/${zipCode}`); Api 1 provide all the addressess
+            // const data = await response.json();
+            //     setLoading(false);
+            //     setLocationData(data);
+            //     setList(true);
